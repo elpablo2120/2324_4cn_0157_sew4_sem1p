63f6352165399d213fa3b77b6ec9b54e16809b0e
elpablo2120 <waldeckerpaul@gmail.com>
2024-03-11 12:21:18 +0100

    UE05 Kasinski Aufgabe: Ceasar - encrypt + decrypt implementiert - nächste Aufgabe crack
    UE06 Comprehensions
    Fertig

--- UE06/src/spellcheck.py
+++ UE06/src/spellcheck.py
@@ -6,15 +6,21 @@ __copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "Development"
 """
-from typing import Set, List, Tuple, Counter
+
+from typing import Set, List, Tuple
 
 
-def read_all_words(filename: str) -> set:
-    '''
+def read_all_words(filename: str) -> set or FileNotFoundError:
+    """
     Liest Wörter aus einer Datei ein und speichert sie in einem Set.
     :param filename: Pfad zur Datei
     :return: set mit allen Wörtern aus der Datei
-    '''
+    >>> read_all_words('de-en.txt')
+    Die Datei de-en.txt wurde nicht gefunden.
+    <class 'FileNotFoundError'>
+    >>> read_all_words('/Users/paulwaldecker/Desktop/HTL3R/4CN/SEW/Angaben/06_py_comprehension/test.txt')
+    {'paul'}
+    """
     word_set = set()
 
     try:
@@ -25,16 +31,22 @@ def read_all_words(filename: str) -> set:
                 word_set.update(words)
     except FileNotFoundError:
         print(f'Die Datei {filename} wurde nicht gefunden.')
+        return FileNotFoundError
 
     return word_set
 
-def split_word(wort:str) -> List[Tuple[str, str]]:
-    '''
+
+def split_word(wort: str) -> List[Tuple[str, str]]:
+    """
     Teilt ein Wort in alle möglichen Paare von Wörtern auf. Und speichert sie als Tupel in einer Liste.
     :param wort: Wort das aufgeteilt werden soll
     :return: Liste mit Tupeln von Wörtern
-    '''
+    >>> split_word('Paul')
+    [('', 'paul'), ('p', 'aul'), ('pa', 'ul'), ('pau', 'l'), ('paul', '')]
+    """
+    wort = wort.lower()
     return [(wort[:i], wort[i:]) for i in range(len(wort) + 1)]
+
 
 def edit1(wort: str) -> Set[str]:
     """
@@ -51,74 +63,74 @@ def edit1(wort: str) -> Set[str]:
     transpose_two = {head + tail[1] + tail[0] + tail[2:] for head, tail in word_splits if len(tail) > 1}
 
     # Fall c) Ein Buchstabe ersetzt
-    replace_one = {head + char + tail[1:] for head, tail in word_splits for char in 'abcdefghijklmnopqrstuvwxyz' if len(tail) > 1}
+    replace_one = {head + char + tail[1:] for head, tail in word_splits for char in 'abcdefghijklmnopqrstuvwxyz' if
+                   len(tail) > 1}
 
     # Fall d) Ein Buchstabe eingefügt
     insert_one = {head + char + tail for head, tail in word_splits for char in 'abcdefghijklmnopqrstuvwxyz'}
 
-    # Kombiniere alle Möglichkeiten in einem Set und gebe es zurück
     result_set = delete_one | transpose_two | replace_one | insert_one
     return result_set
 
-def edit1_good(wort: str, alle_woerter:List[str]) -> Set[str]:
-    '''
+
+def edit1_good(wort: str, alle_woerter: List[str]) -> Set[str]:
+    """
     Ruft edit1 auf und filtert nach Wörtern die in der Liste alle_woerter enthalten sind.
-    :param wort:
-    :param alle_woerter:
-    :return:
-    '''
+    :param wort: Das Eingangswort.
+    :param alle_woerter: Das Wörterbuch.
+    :return: Set mit Wörtern, die auch in der Liste alle_woerter enthalten sind.
+    >>> alle_woerter = read_all_words('/Users/paulwaldecker/Desktop/HTL3R/4CN/SEW/Angaben/06_py_comprehension/de-en.txt')
+    >>> edit1_good('Haalo', alle_woerter)
+    {'hallo'}
+    """
     edit1_result = edit1(wort.lower())
 
-    # Filtern der Ergebnisse, um nur die Wörter im Wörterbuch zu behalten
     return set(edit1_result) & set(alle_woerter)
 
+
 def edit2_good(wort: str, alle_woerter: List[str]) -> Set[str]:
-    '''
+    """
     Finde alle Wörter mit Edit-Distanz zwei. Dazu wird edit1 zweimal aufgerufen.
     :param wort: Das Eingangswort.
     :param alle_woerter: Die Liste aller Wörter.
     :return: Menge aller Wörter mit Edit-Distanz zwei.
-    '''
-    # Step 1: Calculate edit distance one for the input word
+    >>> alle_woerter = read_all_words('/Users/paulwaldecker/Desktop/HTL3R/4CN/SEW/Angaben/06_py_comprehension/de-en.txt')
+    >>> edit2_good('Haaloo', alle_woerter)
+    {'hallo'}
+    """
     edit1_result = edit1(wort.lower())
 
-    # Step 2: Calculate edit distance one for each word in the edit1_result
     edit2_result = set()
     for word in edit1_result:
         edit2_result.update(edit1(word.lower()))
 
-    # Step 3: Filter the results to keep only the words in the word list
     valid_edit2_words = set(edit2_result) & set(alle_woerter)
 
     return valid_edit2_words
 
 def correct(word: str, alle_woerter: List[str]) -> Set[str]:
-    '''
+    """
     Findet Korrekturen in einer Liste.
     Entweder:
     - das Wort ist im Wörterbuch (Ergebnis: eine Liste mit einem Eintrag word)
     - oder (mindestens) ein Wort mit Edit-Distanz eins ist im Wörterbuch (Ergebnis: Liste dieser Wörter)
     - oder (mindestens) ein Wort mit Edit-Distanz zwei ist im Wörterbuch (Ergebnis: Liste dieser Wörter)
     - oder wir haben keine Idee (zu viele Fehler oder unbekanntes Wort): liefere eine Liste mit dem ursprünglichen Wort
-    :param word:
-    :param alle_woerter:
-    :return:
-
-    '''
+    :param word: Das Eingangswort.
+    :param alle_woerter: Das Wörterbuch.
+    :return: Mögliche Korrekturen.
+    >>> alle_woerter = read_all_words('/Users/paulwaldecker/Desktop/HTL3R/4CN/SEW/Angaben/06_py_comprehension/de-en.txt')
+    >>> correct("Aalsuppe", alle_woerter)
+    {'aalsuppe'}
+    >>> correct("Alsuppe", alle_woerter)
+    {'aalsuppe'}
+    >>> sorted(correct("Alsupe", alle_woerter))
+    ['aalsuppe', 'absude', 'alse', 'lupe']
+    """
+    word = word.lower()
     if word in alle_woerter:
         return {word}
-
-    corrections_edit1 = edit1_good(word, alle_woerter)
-
-    corrections_edit2 = edit2_good(word, alle_woerter)
-
-    return corrections_edit1 or corrections_edit2 or {word}
-
-
-
-
-filename = '/Users/paulwaldecker/Desktop/HTL3R/4CN/SEW/Angaben/06_py_comprehension/de-en.txt'
-word_set = read_all_words(filename)
-print(correct('alsupe', word_set))
-print(edit2_good('alsupe', word_set))
-
+    else:
+        corrections_edit1 = edit1_good(word, alle_woerter)
+        corrections_edit2 = edit2_good(word, alle_woerter)
+        return corrections_edit1 or corrections_edit2 or {word}
