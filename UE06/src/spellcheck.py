ed81c684a99e9d0b6e13a8f7e301fddd42c4529b
elpablo2120 <waldeckerpaul@gmail.com>
2024-03-11 09:10:08 +0100

    UE05 Kasinski Aufgabe: Ceasar - encrypt + decrypt implementiert - nächste Aufgabe crack
    UE06 Comprehensions
    Aufgabe: C.3
    - ende aufgabe C.2
    - start aufgabe C.3

--- UE06/src/spellcheck.py
+++ UE06/src/spellcheck.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "Development"
 """
-from typing import Set
+from typing import Set, List, Tuple
 
 
 def read_all_words(filename: str) -> set:
@@ -28,10 +28,16 @@ def read_all_words(filename: str) -> set:
     return word_set
 
 def split_word(wort:str) -> List[Tuple[str, str]]:
-
+    '''
+    Teilt ein Wort in alle möglichen Paare von Wörtern auf. Und speichert sie als Tupel in einer Liste.
+    :param wort: Wort das aufgeteilt werden soll
+    :return: Liste mit Tupeln von Wörtern
+    '''
+    return [(wort[:i], wort[i:]) for i in range(len(wort) + 1)]
 
 # Beispielaufruf:
 filename = '/Users/paulwaldecker/Desktop/HTL3R/4CN/SEW/Angaben/06_py_comprehension/de-en.txt'
 word_set = read_all_words(filename)
-print(word_set)
+print(split_word('hallo'))
+#print(word_set)
 
