19bc6f929fd2353cf8c290d92bdc9cf1d80bcaac
elpablo2120 <waldeckerpaul@gmail.com>
2024-05-06 10:29:36 +0200

    Üben für Test

---
+++ UE_PLF2/src/test.py
@@ -0,0 +1,22 @@
+class Test:
+    def __init__(self, text):
+        self._text = text
+
+    # Getter
+    @property
+    def text(self):
+        return self._text
+
+    # Setter
+    @text.setter
+    def text(self, newText):
+        self._text = newText
+
+    def __str__(self):
+        return f"{self.text}"
+
+
+if __name__ == '__main__':
+    t1 = Test("Hallo")
+    t1.text = "Welt"
+    print(t1)
