79a7d143f3b156499df226b93a7d5eecb04d14fb
elpablo2120 <waldeckerpaul@gmail.com>
2024-06-13 10:53:33 +0200

    UE11 In Progress rsa.py

--- UE11/src/pow.py
+++ UE11/src/pow.py
@@ -4,7 +4,7 @@ __email__ = "0157@htl.rennweg.at"
 __version__ = "0.1"
 __copyright__ = "Copyright 2024"
 __license__ = "GPL"
-__status__ = "In Progress"
+__status__ = "Ready to Review"
 """
 import time
 
---
+++ UE11/src/rsa.py
@@ -0,0 +1,8 @@
+"""
+__author__ = "Paul Waldecker"
+__email__ = "0157@htl.rennweg.at"
+__version__ = "0.1"
+__copyright__ = "Copyright 2024"
+__license__ = "GPL"
+__status__ = "In Progress"
+"""
